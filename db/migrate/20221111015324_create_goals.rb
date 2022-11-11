ae148b49609184781de831cdfd2bbc413a71695e
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-11-10 19:56:28 -0600

    Add models to app

---
+++ db/migrate/20221111015324_create_goals.rb
@@ -0,0 +1,13 @@
+class CreateGoals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :goals do |t|
+      t.string :name, null: false
+      t.text :description
+      t.boolean :finish, default: false
+      t.string :finish_at
+      t.references :list, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
