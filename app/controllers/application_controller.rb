ea909640a7538fabe8b5bfbca3014a2b26b8d58c
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-11-10 21:00:58 -0600

    Add application controller config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  include ActionController::Serialization
+  before_action :require_login!
+
+  def require_login!
+    current_user || render_unauthorized('Access denied')
+  end
+
+  def current_user
+    @current_user ||= authenticate_token
+  end
+
+  def render_unauthorized(error_message)
+    render json: { errors: error_message }, status: :unauthorized
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { message: e.message }, status: :not_found
+  end
+
+  private
+
+  def authenticate_token
+    authenticate_with_http_token do |token, _options|
+      User.where(token:).first
+    end
+  end
 end
