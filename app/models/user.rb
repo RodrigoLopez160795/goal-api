872665234f7660cf1a4286731b5c8cba0f202fa6
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-11-10 20:12:11 -0600

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ApplicationRecord
   belongs_to :list
+  has_secure_password
+  has_secure_token
+  validates :username, :email, :password, presence: true
+  validates :username, length: { in: 6..20 }, presence: true
+  validates :email, :username, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/, message: 'invalid email' }
+
+  def invalidate_token
+    update(token: nil)
+  end
+
+  def self.authenticate(email = nil, username = nil, password)
+    user = User.find_by(email:) unless email.nil?
+    user = User.find_by(username:) unless username.nil?
+    return false unless user&.authenticate(password)
+
+    user.regenerate_token
+    user
+  end
 end
