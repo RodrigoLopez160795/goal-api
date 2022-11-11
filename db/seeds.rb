fdcddcc283e63812480031f334169942ca057c16
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-11-10 20:16:56 -0600

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+List.create(name: "Maho's goals")
+User.create(username: 'Bebesaurio', email: 'rodrigo.lopez.160795@hotmail.com', password: '123remaresbueno', list_id: 1)
+User.create(username: 'Bebesauria', email: 'mahoganycastellanos@hotmail.com', password: 'bieberlandia123', list_id: 1)
